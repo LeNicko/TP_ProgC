da35565453f953a089f003813ea87475f08e1dfd
LeNicko <159636950+LeNicko@users.noreply.github.com>
2025-04-04 10:48:56 +0200

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
 
-int main() {
-    int j = 5;
-
-    for (int i = 0; i < j; i++) {
-        for (int k = 0; k <= i; k++) {
-            if (k == 0 || k == i || i == j - 1) { 
-                printf("* ");
-            } else {
-                printf("# ");
-            }
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
