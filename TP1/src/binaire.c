9ba0c45218c60b781a44d9569211eb52f990077c
LeNicko <159636950+LeNicko@users.noreply.github.com>
2025-04-04 12:08:34 +0200

    Update binaire.c

---
+++ TP1/src/binaire.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int nombres[] = {1, 2, 3, 4, 5};
+    int taille = sizeof(nombres) / sizeof(nombres[0]);
+
+    for (int i = 0; i < taille; i++) {
+        printf("Le nombre %d en binaire est : ", nombres[i]);
+
+        for (int j = 8; j >= 0; j--) {
+            int bit = (nombres[i] >> j) & 1;
+            printf("%d", bit);
+            if (j % 4 == 0) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
