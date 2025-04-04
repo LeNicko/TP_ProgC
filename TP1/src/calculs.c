f9d7b913552a4077499d2fec5a32493aff04ec69
LeNicko <159636950+LeNicko@users.noreply.github.com>
2025-04-04 11:43:07 +0200

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -3,6 +3,35 @@
 int main() {
     int a = 5;
     int b = 8;
-    int somme = a+b;
-  printf(" la somme des chiffres est égale a %d", somme);
+    int résultat = + ;
+switch(résultat) {
+        case '+':
+            printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("%d / %d = %f\n", num1, num2, (float)num1 / num2);
+            } else {
+                printf("Erreur : division par zéro.\n");
+            }
+            break;
+        case '%':
+            printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+            break;
+        case '&':
+            printf("%d & %d = %d\n", num1, num2, num1 & num2);
+            break;
+        case '|':
+            printf("%d | %d = %d\n", num1, num2, num1 | num2);
+            break;
+        case '~':
+            printf("~%d = %d\n", num1, ~num1); 
+            break;
+       
 }
