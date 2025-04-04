c214a343d0a4d701ff92ace9552a1b9de7131360
LeNicko <159636950+LeNicko@users.noreply.github.com>
2025-04-04 09:11:27 +0200

    Update conditions.c

---
+++ TP1/src/conditions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a = 5;
+    int b = 9;
+    int oui;
+  if (a < b){
+    printf("Le chiffre les plus haut est b qui est égale a %d",b);
+  }
+  else{
+     printf("Le chiffre les plus haut est a qui est égale a %d",a);
+  }
+}
