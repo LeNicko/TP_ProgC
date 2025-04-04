13c889dfb93dd7d1461db1865a20d69adad55466
LeNicko <159636950+LeNicko@users.noreply.github.com>
2025-04-04 09:16:04 +0200

    Update cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -4,7 +4,7 @@
 int main() {
     double rayon = 5.00;
     double aire = 3.14*rayon*rayon;
-    double perimetre = 2*3.14*rayon
+    double perimetre = 2*3.14*rayon;
   printf("L'aire est égale à %f", aire);
   printf("Le perimetre est égale à %f", perimetre);
     
