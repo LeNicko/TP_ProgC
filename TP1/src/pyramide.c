08147272a0f450c22fd88c0c2d6b7b2bab1d2bd5
LeNicko <159636950+LeNicko@users.noreply.github.com>
2025-04-04 10:35:53 +0200

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-  int j = 5;
-  int boucle;
-  for (boucle =0;boucle=j;boucle++)
-    {
-      for (int i=0;i<j;i++)
-    {
-    printf("*");
-    }
-      printf(" \n");
+    int j = 5;
+    int boucle;
+    
+    for (boucle = 0; boucle < j; boucle++) { 
+        for (int i = 0; i < j; i++) {
+            printf("*");
+        }
+        printf("\n"); 
     }
 
-  }
+    return 0;
+}
