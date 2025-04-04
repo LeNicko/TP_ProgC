35aca57698e0a5d2219d5e5e4ccc3e23163c0f53
LeNicko <159636950+LeNicko@users.noreply.github.com>
2025-04-04 10:11:22 +0200

    Update opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a = 16;
+    int b = 3;
+    int somme = a+b;
+    int soustraction = a-b;
+    int division = a/b;
+    int multiplicaton = a*b;
+    int modulo = a/b;
+  if (a == b )
+  {
+    printf (TRUE);
+  }
+  else{
+    printf (FALSE)
+  }
+  if (a>=b)
+   {
+    printf (TRUE);
+  }
+  else{
+    printf (FALSE)
+  }
+  printf ("La somme = %d",somme);
+  printf ("La soustration = %d",soustraction);
+  printf ("La division = %d",division);
+  printf ("La multiplication = %d",multiplication);
+  printf ("La modulo = %d",modulo);
+
+
+}
