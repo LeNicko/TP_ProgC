1c67b2045849ed9f451b9fbad4ea7bd23426ca94
LeNicko <159636950+LeNicko@users.noreply.github.com>
2025-04-04 11:11:59 +0200

    Update conditions.c

--- TP1/src/conditions.c
+++ TP1/src/conditions.c
@@ -3,16 +3,19 @@
 int main() {
     int somme = 0;
 
-    for (int i = 1; i = 1000; i++) {
-        if (i % 11 == 0)
-            printf("ignoré car divisible par 11");
+    for (int i = 1; i <= 1000; i++) {
+        if (i % 11 == 0) {
+            continue;
+        }
 
-        if (i % 5 == 0 || i % 7 == 0)
+        if (i % 5 == 0 || i % 7 == 0) {
+            if (somme + i > 5000) {
+                break;
+            }
             somme += i;
-
-        if (somme > 5000)
-            break;
+        }
     }
 
     printf("Somme finale : %d\n", somme);
+    return 0;
 }
