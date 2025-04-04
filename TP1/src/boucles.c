ee510088e4b3425bc8867e3b979097f56eaf382a
LeNicko <159636950+LeNicko@users.noreply.github.com>
2025-04-04 10:49:10 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int a = 5;
-    for (int i=0;i<a; i++)
-      {
-      printf("Bonjour le monde.");
+    int j = 5;
+
+    for (int i = 0; i < j; i++) {
+        for (int k = 0; k <= i; k++) {
+            if (k == 0 || k == i || i == j - 1) { 
+                printf("* ");
+            } else {
+                printf("# ");
+            }
+        }
+        printf("\n");
     }
+
+    return 0;
 }
