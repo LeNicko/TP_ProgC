89533c8c548511fa2bdca5e77fd5bbbdde3c10c4
LeNicko <159636950+LeNicko@users.noreply.github.com>
2025-04-18 12:21:56 +0200

    Update tri.c

---
+++ TP3/src/tri.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int tab[100];
+
+    srand(time(NULL));
+    for (int i = 0; i < 100; i++) {
+        tab[i] = rand() % 2001 - 1000; // Valeurs entre -1000 et 1000
+    }
+
+    printf("Tableau non trié :\n");
+    for (int i = 0; i < 100; i++) {
+        printf("%d ", tab[i]);
+    }
+    printf("\n");
+
+   
+    for (int i = 0; i < 99; i++) {
+        for (int j = 0; j < 99 - i; j++) {
+            if (tab[j] > tab[j + 1]) {
+                int tmp = tab[j];
+                tab[j] = tab[j + 1];
+                tab[j + 1] = tmp;
+            }
+        }
+    }
+
+    printf("Tableau trié par ordre croissant :\n");
+    for (int i = 0; i < 100; i++) {
+        printf("%d ", tab[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
